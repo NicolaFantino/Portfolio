4758451e863daebec6630f7ce261aa2c4b88b856
Nicola Fantino <nicola.fantino@outlook.com>
2025-10-26 20:59:35 +0100

    Changed name, title and description in the config.ts file

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
-  name: "Ryan Fitzgerald",
-  title: "Senior Software Engineer",
-  description: "Portfolio website of Ryan Fitzgerald",
+  name: "Nicola Fantino",
+  title: "Digital Media Engineer",
+  description: "Portfolio website of Nicola Fantino",
   accentColor: "#1d4ed8",
   social: {
     email: "your-email@example.com",
